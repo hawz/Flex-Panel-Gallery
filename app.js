c0a6f58c75d20a1b126af9e18e5eaa17abfb2dea
Alberto Masia <alberto.masia@gmail.com>
2019-02-01 20:02:13 +0100

    Create Flex Panel Gallery with horizontal and vertical style options.

---
+++ app.js
@@ -0,0 +1,13 @@
+
+    // const panels = document.querySelectorAll('.panel');
+    // panels.forEach(panel => panel.addEventListener('click', toggleOpen));
+    // panels.forEach(panel => panel.addEventListener('transitionend', toggleOpenActive));
+
+    // function toggleOpen() {
+    //   this.classList.toggle('open');
+    // }
+    // function toggleOpenActive(e) {
+    //   if (e.propertyName.includes('flex')) {
+    //     this.classList.toggle('open-active');
+    //   }
+    // }
