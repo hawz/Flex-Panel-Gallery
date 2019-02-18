47e26749676459b6bfdf50928d01f0bd39d0a953
Alberto Masia <alberto.masia@gmail.com>
2019-02-18 19:41:56 +0100

    Fix class toggle method.

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
+(function() {
+  const panel = document.querySelectorAll(".panel");
+  panel.forEach(panel => panel.addEventListener("click", toggleOpen));
+  panel.forEach(panel => panel.addEventListener("transitionend", toggleActive));
 
-    // const panels = document.querySelectorAll('.panel');
-    // panels.forEach(panel => panel.addEventListener('click', toggleOpen));
-    // panels.forEach(panel => panel.addEventListener('transitionend', toggleOpenActive));
+  function toggleOpen() {
+    this.classList.toggle("open");
+  }
 
-    // function toggleOpen() {
-    //   this.classList.toggle('open');
-    // }
-    // function toggleOpenActive(e) {
-    //   if (e.propertyName.includes('flex')) {
-    //     this.classList.toggle('open-active');
-    //   }
-    // }
+  function toggleActive(e) {
+    if (e.propertyName.includes("flex")) {
+      this.classList.toggle("open-active");
+    }
+  }
+})();
